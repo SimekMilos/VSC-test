421aed424d67e2ba3e9f07931d44e618142c68a5
Miloš Šimek <tjsnake@me.com>
2026-03-31 12:05:50 +0200

    Initial commit

---
+++ sample.js
@@ -0,0 +1,11 @@
+console.log("----------------------------------------");
+console.log("Sample JavaScript File");
+console.log("----------------------------------------");
+
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+const person = "Miloš";
+greet("World");
+greet("JavaScript");
+console.log("----------------------------------------");
